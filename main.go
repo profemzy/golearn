91e0d71d5d34bd47cbe5ec4856468d9a0dff070f
Femi Oladele <profemzy@gmail.com>
2020-12-18 07:52:55 +0100

    Added Some Tuts

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
-var (
-	actorName = "James Bond"
-	companion = "Sarah Smith"
-	age       = 17
-	i         = 5
-)
-
 func main() {
-	i = 30
-	j := 42.5
-	fmt.Println(i + age)
-	fmt.Println(actorName + " " + companion)
-	fmt.Printf("%v, %T\n", j, j)
 
-	var k float32
-	k = float32(i)
-	fmt.Printf("%v, %T\n", k, k)
-
-	var textAge string
-	textAge = strconv.Itoa(age)
-	fmt.Printf("%v, %T\n", textAge, textAge)
 }
